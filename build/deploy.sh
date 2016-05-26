be8e145f70f80d689d491d8e87c029acb304d93f
Gordon Stratton <gordon@rightangle.cc>
2016-05-26 21:20:24 +0000

    Update deployment script based on changes in s3_website
    We also use "$@" to allow command line arguments to be specified to the script,
    useful for `--force`.

--- build/deploy.sh
+++ build/deploy.sh
@@ -2,4 +2,7 @@
 
 BASEPATH=$(cd "$(dirname "$0")/.."; pwd -P)
 
-s3_website push --config_dir ${BASEPATH}/config --site ${BASEPATH}/target/_site
+s3_website push \
+  --config-dir ${BASEPATH}/config \
+  --site ${BASEPATH}/target/_site \
+  "$@"
